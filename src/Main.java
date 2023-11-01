f6f35c8aaf386659fcd88a18d36a54a8a831ff12
smzuluaga01home <santimzuluaga01@hotmail.com>
2023-11-01 12:05:39 -0500

    ajustes de comentarios de código

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,14 @@ public class Main {
         /*Bienvenidos a la prueba de Inglés de Duolingo
             Para la correcta ejecición del código, debe seguir las siguientes instrucciones:
                 1. Elegir un tipo de diccionario: 0 -> español ó 1 -> inglés.
-                2. Si se inicializa un diccionario en español, las palabras evaluadas saldrán en español y deberás
-                    responder su significado en ingles.
-                2. Si se inicializa un diccionario en inglés, las palabras evaluadas saldrán en inglés y deberás
-                    responder su significado en español.
-                3. Cuando su respuesta sea en español, ¡LA ORTOGRAFÍA CUENTA! las tíldes hacen la diferencia entre un acierto
-                    y un error.
-                4. Puede recibir palabras repetidas en su evaluación.
+                2. Usted puede elegir el nivel de exigencia de la prueba al elegir la cantidad de palabras a evaluar entre 1 y 50.
+                3. Si se inicializa un diccionario en español, las palabras evaluadas saldrán en español y deberás
+            responder su significado en ingles.
+                4. Si se inicializa un diccionario en inglés, las palabras evaluadas saldrán en inglés y deberás
+            responder su significado en español.
+                5. Cuando su respuesta sea en español, ¡LA ORTOGRAFÍA CUENTA! las tíldes hacen la diferencia entre un acierto
+            y un error.
+                6. Puede recibir palabras repetidas en su evaluación.
         */
 
         //Solicitud de tipo de diccionario para inicializar el mismo
